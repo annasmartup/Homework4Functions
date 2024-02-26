1bbb087ea96da56d19f91c73504b1f9e05c79ab6
Anna_skazka <smartupfit@yandex.ru>
2024-02-27 01:54:06 +1000

    Выполнено первое задание

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        InputNumber();
+    }
+
+    static void InputNumber()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое число, или q, чтобы выйти. Также программа будет завершена автоматически, если сумма цифр числа будет четной: ");
+            string input = Console.ReadLine();
+            if (input == "q")
+            {
+                break;
+            }
+            int number;
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Неверный формат числа. Введите целое число или q для выхода");
+                continue;
+            }
+            if (IsDigitSumEven(number))
+            {
+                break;
+            }
+        }
+    }
+
+    static bool IsDigitSumEven(int number)
+    {
+        List<int> digits = new List<int>();
+
+        while (number > 0)
+        {
+            digits.Add(number % 10);
+            number /= 10;
+        }
+        int sum = 0;
+        foreach(int digit in digits)
+        {
+            sum += digit;
+        }
+        return sum % 2 ==0;
+    }
+}
