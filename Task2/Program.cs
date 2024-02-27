1f620650ca95c82de01c209fcfcaf4c32576ed5f
Anna_skazka <smartupfit@yandex.ru>
2024-02-27 12:34:21 +1000

    Сделали все 3 задания

---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = CreateRandomArray(7);
+        ShowArray(array);
+        Console.WriteLine("Количество четных чисел в массиве: " + CountEven(array));
+    }
+    static int[] CreateRandomArray (int size)
+    {
+        int[] array = new int [size];
+        for(int i = 0; i < size; i++)
+        {
+            array[i] = new Random().Next(100, 1000);
+        }
+        return array;
+    }
+    static void ShowArray(int[] array)
+{
+    foreach(int item in array)
+    {
+        Console.Write(item+", ");
+    }
+    Console.WriteLine();
+}
+    static int CountEven(int[] array)
+    {
+        int count = 0;
+        foreach(int number in array)
+        {
+            if(number % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = {43, 8, 1, 504};
+        ReverseArray(numbers);
+        ShowArray(numbers);
+    }
+    static int[] ReverseArray(int[] numbers)
+    {
+        for(int i = 0; i < numbers.Length / 2; i++)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - 1 - i];
+            numbers[numbers.Length - 1 - i] = temp;
+            
+        }
+        return numbers;
+    }
+        static void ShowArray(int[] array)
+{
+    foreach(int item in array)
+    {
+        Console.Write(item+", ");
+    }
+}
+}
